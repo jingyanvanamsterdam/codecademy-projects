584d2e4a842289fcf99499d9bf98e9e59a705fae
jingyan <jingyan.dong420@gmail.com>
2023-11-09 17:53:16 +0100

    add useNavigation and Navigate to not found page

--- starting_code/src/App.js
+++ starting_code/src/App.js
@@ -14,6 +14,7 @@ const appRouter = createBrowserRouter(createRoutesFromElements(
     <Route path=":type" element={<HomePage/>} />
     <Route path=":type/:id" element={<PetDetailsPage/>} />
     <Route path="search" element={<SearchPage/>} />
+    <Route path="pet-details-not-found" element={<PetDetailsNotFound/>} />
   </Route>
 ));
 
@@ -24,19 +25,3 @@ function App() {
 }
 
 export default App;
-
-/*
-Your objective will be to add client-side routing to the application using React Router so that:
-
-The HomePage component responds to the browser’s current URL by displaying only pets of the species the user wishes to view.
-
-The PetDetailsPage page displays when the browser’s current URL includes a specific pet’s id.
-
-The PetDetailsPage displays data for the correct pet based on the id in the URL parameters’ values.
-
-When the user searches for a pet in the search bar, they are redirected to the SearchPage, which uses the query parameter called name to filter pets by name.
-
-When a user clicks a pet whose details are not available, they are redirected to a PetNotFoundPage.
-
-From the PetNotFound page, users can click “Go Home” button that will take them to the root path page.
-*/
--- starting_code/src/pages/petDetailsNotFound/index.js
+++ starting_code/src/pages/petDetailsNotFound/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 // import useNavigate here.
 
 const PetDetailsNotFound = () => {
 
   // get the navigate function from useNavigate
-  const navigate = "REPLACE ME";
+  const navigate = useNavigate();
 
   const goHome = () => {
-    // Go home!
+    navigate("/")
   }
-  
+
   return (
     <main className="page">
       <h3>404: Who let the dogs out?</h3>
